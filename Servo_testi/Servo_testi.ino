19541227d2229ad523b60816aa14586939b235d3
Teemu <teemu.kivinen@edu.turkuamk.fi>
2023-11-07 15:48:25 +0200

    lisätty toimiva versio kansion sisälle

--- Servo_testi/Servo_testi.ino
+++ Servo_testi/Servo_testi.ino
@@ -1,29 +1,49 @@
-#include <Servo.h>
-
-int pos = 90;
+#include <Servo.h> //include server library
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
-void setup()
-{
-  servo1.attach(9);
-  servo2.attach(10);
-  servo3.attach(11);
+  //here we create servo objects to control a servo
+
+int pos = 90; //initial position of servos
+
+char val; //initial value of input
+
+void setup(){
+  Serial.begin(9600); //serial comm begin at 9600bps
+
+  servo1.attach(9); //servo connected to pin 9 
+  servo2.attach(10); //servo connected to pin 10
+  servo3.attach(11); //servo connected to pin 11
 }
 
-void loop()
-{
-  servo1.write(90);
-  delay(150);
+void loop(){
 
-  servo2.write(130);
-  delay(1500);
-  
-  servo2.write(0);
-  delay(1500);
+  if(Serial.available()){ //if serial value is available
+    val = Serial.read(); //then read the serial value
+    if(val == 'd'){ //if value input is d
+        pos += 20; //servo position increases by 20 (CCW)
+        servo1.write(pos); //servo moves according to position
+        delay(15); //delay for the servo to get into the position
 
-  servo3.write(90);
-  delay(15);
+        servo2.write(pos);
+        delay(15);
+        
+        servo3.write(pos);
+        delay(15);
+    }
+    if(val == 'a'){
+        pos -= 20; //servo position decreases by 20 (CCW)
+        servo1.write(pos); //servo moves according to position
+        delay(15); //delay for the servo to get into the position
+
+        servo2.write(pos);
+        delay(15);
+        
+        servo3.write(pos);
+        delay(15);
+    }
   }
+}
+
