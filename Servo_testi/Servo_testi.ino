cdc6bccd4b4aace3f412088666bb06aee6c86624
Teemu <teemu.kivinen@edu.turkuamk.fi>
2023-11-08 15:37:54 +0200

    reroll to the last working version

--- Servo_testi/Servo_testi.ino
+++ Servo_testi/Servo_testi.ino
@@ -9,7 +9,6 @@ Servo servo3;
 int pos = 90; //initial position of servos
 
 char val; //initial value of input
-char t; //another parameter for values
 
 void setup(){
   Serial.begin(9600); //serial comm begin at 9600bps
@@ -23,33 +22,27 @@ void loop(){
 
   if(Serial.available()){ //if serial value is available
     val = Serial.read(); //then read the serial value
-    if(val == 'w'){ //if value input is d
-        t = Serial.read(); //read the serial value after FWD or BWD movement
-        pos += 90; //servo position increases by 20 (CCW)
+    if(val == 'd'){ //if value input is d
+        pos += 20; //servo position increases by 20 (CCW)
         servo1.write(pos); //servo moves according to position
         delay(15); //delay for the servo to get into the position
-        if(t == 'a'){
+
         servo2.write(pos);
         delay(15);
-        }
-        if(t == 'd'){
+        
         servo3.write(pos);
         delay(15);
-        }
     }
-    if(val == 's'){
-        t = Serial.read(); //get the value of left or right
-        pos -= 90; //servo position decreases by 20 (CCW)
+    if(val == 'a'){
+        pos -= 20; //servo position decreases by 20 (CCW)
         servo1.write(pos); //servo moves according to position
         delay(15); //delay for the servo to get into the position
-        if(t == 'a'){
+
         servo2.write(pos);
         delay(15);
-        }
-        if(t == 'd'){
+        
         servo3.write(pos);
         delay(15);
-        }
     }
   }
 }
